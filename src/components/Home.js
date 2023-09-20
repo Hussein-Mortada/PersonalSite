57e74d54b6dfe57d93fd40b018dc233958dfaa33
Hussein Mortada <122917015+Hussein-Mortada@users.noreply.github.com>
2023-09-20 01:13:54 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(){
             <h3 className="hello">Hello, I'm</h3>
             <h1 className="name">Hussein Mortada.</h1>
             <h1 className="title">Looking For Work</h1>
-            <p className="small-desc">I am a software developer.  I work in Java, Python, JavaScript.  I develop web applications or REST API's.  Feel free to connect with me or 
+            <p className="small-desc">I am a software developer.  I work with Java, Python, JavaScript and others. I develop software or web applications.  Feel free to connect with me or 
             check out my projects! :) </p>
         </div>
     );
